4b28a36e4d404055d6fea579cd1511481142601f
mprTest1 <98311505+mprTest1@users.noreply.github.com>
2023-02-13 11:52:26 +0100

    0

--- iads/Classifiers.py
+++ iads/Classifiers.py
@@ -14,3 +14,250 @@ import numpy as np
 import pandas as pd
 
 # ---------------------------
+class Classifier:
+    """ Classe (abstraite) pour représenter un classifieur
+        Attention: cette classe est ne doit pas être instanciée.
+    """
+    
+    def __init__(self, input_dimension):
+        """ Constructeur de Classifier
+            Argument:
+                - intput_dimension (int) : dimension de la description des exemples
+            Hypothèse : input_dimension > 0
+        """
+        raise NotImplementedError("Please Implement this method")
+        
+    def train(self, desc_set, label_set):
+        """ Permet d'entrainer le modele sur l'ensemble donné
+            desc_set: ndarray avec des descriptions
+            label_set: ndarray avec les labels correspondants
+            Hypothèse: desc_set et label_set ont le même nombre de lignes
+        """        
+        raise NotImplementedError("Please Implement this method")
+    
+    def score(self,x):
+        """ rend le score de prédiction sur x (valeur réelle)
+            x: une description
+        """
+        raise NotImplementedError("Please Implement this method")
+    
+    def predict(self, x):
+        """ rend la prediction sur x (soit -1 ou soit +1)
+            x: une description
+        """
+        raise NotImplementedError("Please Implement this method")
+
+    def accuracy(self, desc_set, label_set):
+        """ Permet de calculer la qualité du système sur un dataset donné
+            desc_set: ndarray avec des descriptions
+            label_set: ndarray avec les labels correspondants
+            Hypothèse: desc_set et label_set ont le même nombre de lignes
+        """
+        # ------------------------------
+        # COMPLETER CETTE FONCTION ICI : 
+        
+        pred_set = [self.predict(i) for i in desc_set]
+        correct = 0.0
+        for i in range(len(label_set)):
+            if label_set[i] == pred_set[i]:
+                correct += 1
+        return correct/len(label_set)
+        
+        # ------------------------------
+
+class ClassifierKNN(Classifier):
+    """ Classe pour représenter un classifieur par K plus proches voisins.
+        Cette classe hérite de la classe Classifier
+    """
+
+    # ATTENTION : il faut compléter cette classe avant de l'utiliser !
+    
+    def __init__(self, input_dimension, k):
+        """ Constructeur de Classifier
+            Argument:
+                - intput_dimension (int) : dimension d'entrée des exemples
+                - k (int) : nombre de voisins à considérer
+            Hypothèse : input_dimension > 0
+        """
+        self.input_dimension = input_dimension
+        self.k = k
+        self.ref_desc = None
+        self.ref_label = None
+        
+    def score(self,x):
+        """ rend la proportion de +1 parmi les k ppv de x (valeur réelle)
+            x: une description : un ndarray
+        """
+        ref_tab = [np.linalg.norm(self.ref_desc[i]-x) for i in range(len(self.ref_label))]
+        ind=np.argsort(np.asarray(ref_tab),axis=0)
+        
+        nb_1 = 0.0
+        for i in range(self.k):
+            if self.ref_label[ind[i]]==1:
+                nb_1 += 1
+        return (nb_1/self.k-0.5)*2   
+    
+    def predict(self, x):
+        """ rend la prediction sur x (-1 ou +1)
+            x: une description : un ndarray
+        """
+        if self.score(x)<=0:
+            return -1
+        return 1
+
+    def train(self, desc_set, label_set):
+        """ Permet d'entrainer le modele sur l'ensemble donné
+            desc_set: ndarray avec des descriptions
+            label_set: ndarray avec les labels correspondants
+            Hypothèse: desc_set et label_set ont le même nombre de lignes
+        """        
+        self.ref_desc = desc_set
+        self.ref_label = label_set
+    
+class ClassifierLineaireRandom(Classifier):
+    """ Classe pour représenter un classifieur linéaire aléatoire
+        Cette classe hérite de la classe Classifier
+    """
+    
+    def __init__(self, input_dimension):
+        """ Constructeur de Classifier
+            Argument:
+                - intput_dimension (int) : dimension de la description des exemples
+            Hypothèse : input_dimension > 0
+        """
+        self.input_dimension = input_dimension
+        v = np.random.uniform(-1,1,input_dimension)
+        self.w = v / np.linalg.norm(v)
+        
+    def train(self, desc_set, label_set):
+        """ Permet d'entrainer le modele sur l'ensemble donné
+            desc_set: ndarray avec des descriptions
+            label_set: ndarray avec les labels correspondants
+            Hypothèse: desc_set et label_set ont le même nombre de lignes
+        """        
+        print("Pas d'apprentissage pour ce classifieur")
+    
+    def score(self,x):
+        """ rend le score de prédiction sur x (valeur réelle)
+            x: une description
+        """
+        return np.dot(x,self.w)
+    
+    def predict(self, x):
+        """ rend la prediction sur x (soit -1 ou soit +1)
+            x: une description
+        """
+        if self.score(x)<=0:
+            return -1
+        return 1
+
+class ClassifierKNN_MC(Classifier):
+
+    # ATTENTION : il faut compléter cette classe avant de l'utiliser !
+    
+    def __init__(self, input_dimension, k, n):
+        self.input_dimension = input_dimension
+        self.k = k
+        self.n = n
+        self.ref_desc = None
+        self.ref_label = None
+        
+    def score(self,x):
+        """ 
+        Enregistrer les étiquettes des k points les plus proches du point prédit et retourner le dictionnaire
+        """
+        ref_tab = [np.linalg.norm(self.ref_desc[i]-x) for i in range(len(self.ref_label))]
+        ind=np.argsort(np.asarray(ref_tab),axis=0)
+        
+        d = dict()
+        for i in range(self.k):
+            if self.ref_label[ind[i]] not in d:
+                d[self.ref_label[ind[i]]] = 1
+            else:
+                d[self.ref_label[ind[i]]] += 1
+        return d   
+    
+    def predict(self, x):
+        d = self.score(x)
+        return max(d, key=lambda k: d[k])
+
+    def train(self, desc_set, label_set):
+        """ Permet d'entrainer le modele sur l'ensemble donné
+            desc_set: ndarray avec des descriptions
+            label_set: ndarray avec les labels correspondants
+            Hypothèse: desc_set et label_set ont le même nombre de lignes
+        """        
+        self.ref_desc = desc_set
+        self.ref_label = label_set
+        
+class ClassifierPerceptron(Classifier):
+    """ Perceptron de Rosenblatt
+    """
+    def __init__(self, input_dimension, learning_rate=0.01, init=True ):
+        """ Constructeur de Classifier
+            Argument:
+                - input_dimension (int) : dimension de la description des exemples (>0)
+                - learning_rate (par défaut 0.01): epsilon
+                - init est le mode d'initialisation de w: 
+                    - si True (par défaut): initialisation à 0 de w,
+                    - si False : initialisation par tirage aléatoire de valeurs petites
+        """
+        self.input_dimension = input_dimension
+        self.learning_rate = learning_rate
+        if init:
+            self.w = np.zeros(input_dimension)
+        else:
+            self.w = (np.random.uniform(0,1,input_dimension)*2-1)*0.001
+        
+    def train_step(self, desc_set, label_set):
+        """ Réalise une unique itération sur tous les exemples du dataset
+            donné en prenant les exemples aléatoirement.
+            Arguments:
+                - desc_set: ndarray avec des descriptions
+                - label_set: ndarray avec les labels correspondants
+        """        
+        verify_list = [i for i in range(len(desc_set))]
+        np.random.shuffle(verify_list)
+        for i in verify_list:
+            if self.predict(desc_set[i]) != label_set[i]:
+                self.w = self.w + desc_set[i]*self.learning_rate*label_set[i]
+        return None
+        
+     
+    def train(self, desc_set, label_set, nb_max=100, seuil=0.001):
+        """ Apprentissage itératif du perceptron sur le dataset donné.
+            Arguments:
+                - desc_set: ndarray avec des descriptions
+                - label_set: ndarray avec les labels correspondants
+                - nb_max (par défaut: 100) : nombre d'itérations maximale
+                - seuil (par défaut: 0.001) : seuil de convergence
+            Retour: la fonction rend une liste
+                - liste des valeurs de norme de différences
+        """
+        export_list = []
+        while nb_max>0:
+            w1 = self.w.copy()
+            self.train_step(desc_set, label_set)
+            w2 = self.w.copy()
+            d = pow(((w1-w2) ** 2).sum(),0.5)
+            export_list.append(d)
+            if d <= seuil:
+                return export_list
+            nb_max -= 1
+        return export_list
+    
+    def score(self,x):
+        """ rend le score de prédiction sur x (valeur réelle)
+            x: une description
+        """
+        return np.dot(x,self.w)
+    
+    def predict(self, x):
+        """ rend la prediction sur x (soit -1 ou soit +1)
+            x: une description
+        """
+        if self.score(x)<=0:
+            return -1
+        return 1
+
+    
--- iads/utils.py
+++ iads/utils.py
@@ -16,3 +16,97 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # ------------------------ 
+# genere_dataset_uniform:
+def genere_dataset_uniform(p, n, binf=-1, bsup=1):
+    """ int * int * float^2 -> tuple[ndarray, ndarray]
+        Hyp: n est pair
+        p: nombre de dimensions de la description
+        n: nombre d'exemples de chaque classe
+        les valeurs générées uniformément sont dans [binf,bsup]
+    """
+    if binf > bsup:
+        raise NotImplementedError("Please Implement this method")
+    return np.random.uniform(binf,bsup,(2*n,p)), np.asarray([-1 for i in range(0, n)] + [1 for i in range(0, n)])
+# genere_dataset_gaussian:
+def genere_dataset_gaussian(positive_center, positive_sigma, negative_center, negative_sigma, nb_points):
+    """ les valeurs générées suivent une loi normale
+        rend un tuple (data_desc, data_labels)
+    """
+   
+    desc_negative = np.random.multivariate_normal(negative_center, negative_sigma, nb_points)
+    desc_positive = np.random.multivariate_normal(positive_center, positive_sigma, nb_points)
+
+    desc = np.concatenate((desc_negative, desc_positive))
+    
+    labels = np.asarray([-1 for i in range(0, nb_points)] + [+1 for i in range(0, nb_points)])
+    
+    return desc, labels
+
+def genere_train_test(desc_set, label_set, n_pos, n_neg):
+    """ permet de générer une base d'apprentissage et une base de test
+        desc_set: ndarray avec des descriptions
+        label_set: ndarray avec les labels correspondants
+        n_pos: nombre d'exemples de label +1 à mettre dans la base d'apprentissage
+        n_neg: nombre d'exemples de label -1 à mettre dans la base d'apprentissage
+        Hypothèses: 
+           - desc_set et label_set ont le même nombre de lignes)
+           - n_pos et n_neg, ainsi que leur somme, sont inférieurs à n (le nombre d'exemples dans desc_set)
+    """
+    desc_pos = [desc_set[i] for i in range(len(desc_set)) if label_set[i]>=0]
+    desc_neg = [desc_set[i] for i in range(len(desc_set)) if label_set[i]<0]
+    pos1 = random.sample(desc_pos, n_pos)
+    neg1 = random.sample(desc_neg, n_neg)
+    for i in pos1:
+        for j in range(len(desc_pos)):
+            if (i==desc_pos[j]).all():
+                desc_pos.pop(j)
+                break
+    for i in neg1:
+        for j in range(len(desc_neg)):
+            if (i==desc_neg[j]).all():
+                desc_neg.pop(j)
+                break
+    desc_appr = np.asarray(pos1+neg1)
+    label_appr = np.asarray([1]*n_pos+[-1]*n_neg)
+    desc_test = np.asarray(desc_pos+desc_neg)
+    label_test = np.asarray([1]*len(desc_pos)+[-1]*len(desc_neg))
+    return (desc_appr,label_appr),(desc_test,label_test)
+        
+
+# plot2DSet:
+def plot2DSet(desc,labels):    
+    """ ndarray * ndarray -> affichage
+        la fonction doit utiliser la couleur 'red' pour la classe -1 et 'blue' pour la +1
+    """
+    data2_negatifs = desc[labels == -1]
+    data2_positifs = desc[labels == +1]
+    plt.scatter(data2_negatifs[:,0],data2_negatifs[:,1],marker='o', color="red") # 'o' rouge pour la classe -1
+    plt.scatter(data2_positifs[:,0],data2_positifs[:,1],marker='x', color="blue") # 'x' bleu pour la classe +1
+# plot_frontiere:
+def plot_frontiere(desc_set, label_set, classifier, step=30):
+    """ desc_set * label_set * Classifier * int -> NoneType
+        Remarque: le 4e argument est optionnel et donne la "résolution" du tracé: plus il est important
+        et plus le tracé de la frontière sera précis.        
+        Cette fonction affiche la frontière de décision associée au classifieur
+    """
+    mmax=desc_set.max(0)
+    mmin=desc_set.min(0)
+    x1grid,x2grid=np.meshgrid(np.linspace(mmin[0],mmax[0],step),np.linspace(mmin[1],mmax[1],step))
+    grid=np.hstack((x1grid.reshape(x1grid.size,1),x2grid.reshape(x2grid.size,1)))
+    
+    # calcul de la prediction pour chaque point de la grille
+    res=np.array([classifier.predict(grid[i,:]) for i in range(len(grid)) ])
+    res=res.reshape(x1grid.shape)
+    # tracer des frontieres
+    # colors[0] est la couleur des -1 et colors[1] est la couleur des +1
+    plt.contourf(x1grid,x2grid,res,colors=["darksalmon","skyblue"],levels=[-1000,0,1000])
+
+def create_XOR(n, var):
+    """ int * float -> tuple[ndarray, ndarray]
+        Hyp: n et var sont positifs
+        n: nombre de points voulus
+        var: variance sur chaque dimension
+    """
+    desc1,label1 = genere_dataset_gaussian(np.array([1,1]),np.array([[var,0],[0,var]]),np.array([0,0]),np.array([[var,0],[0,var]]),n)
+    desc2,label2 = genere_dataset_gaussian(np.array([0,1]),np.array([[var,0],[0,var]]),np.array([1,0]),np.array([[var,0],[0,var]]),n)
+    return np.concatenate((desc1,desc2),axis=0),np.asarray([-1]*len(desc1)+[1]*len(desc2))
